561b5e1eb673e20004d9cf3bd671cbcdc024f8d4
Jacob Stolker <stolkerja@gmail.com>
2022-09-06 05:54:51 +0000

    modify dev

--- dev/main.cpp
+++ dev/main.cpp
@@ -14,10 +14,11 @@ int main(int argc, char** argv)
   }
   string strMsg = utils::readFile("./dev/files/test.txt");
   cout << strMsg << endl;
-  AES aes(AESKeyLength::AES_128);
-  string key = "123456789abcdef0";
-  for (char c : key) {
-    cout << stoi(c) << endl;
+  AES aes(AESKeyLength::AES_256);
+  string key = "123456789abcdef0123456789abcdef0";
+  unsigned char *key = new char[]
+  for (unsigned int i = 0; i < key.length(); i += 2) {
+    cout << stoi(key.substr(i, 2), nullptr, 16) << endl;
   }
   // const char* msg = strMsg.c_str();
   // unsigned char c = aes.EncryptECB(msg, strMsg.length(), key);
